406b976d1989d5fa4acdb387cfd827aa866b1be4
Tariq-SOF <tari.almutairi1995@gmai.com>
2023-11-06 16:27:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+let channel = document.getElementById("one");
+
+let img1 = document.getElementById("img");
+
+channel.addEventListener("click", function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+    
+});
+
+
+let channe2 = document.getElementById("two");
+
+let img2 = document.getElementById("img");
+
+channe2.addEventListener("click", function() {
+    img2.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    
+});
+
+let channe3 = document.getElementById("three");
+
+let img3 = document.getElementById("img");
+
+channe3.addEventListener("click", function() {
+    img3.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
+    
+});
+
+let channe4 = document.getElementById("four");
+
+let img4 = document.getElementById("img");
+
+channe4.addEventListener("click", function() {
+    img4.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
+    
+});
+
+let channe5 = document.getElementById("five");
+
+let img5 = document.getElementById("img");
+
+channe5.addEventListener("click", function() {
+    img5.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+    
+});
+
+let channe6 = document.getElementById("six");
+
+let img6 = document.getElementById("img");
+
+channe6.addEventListener("click", function() {
+    img6.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
+    
+});
+
+let channe7 = document.getElementById("seven");
+
+let img7 = document.getElementById("img");
+
+channe7.addEventListener("click", function() {
+    img7.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    
+});
+
+let channe8 = document.getElementById("eight");
+
+let img8 = document.getElementById("img");
+
+channe8.addEventListener("click", function() {
+    img8.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg";
+    
+});
+
+let channe9 = document.getElementById("nine");
+
+let img9 = document.getElementById("img");
+
+
+channe9.addEventListener("click", function() {
+    img9.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg";
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
